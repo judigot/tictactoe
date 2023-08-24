d3c9bcee9979698299733a83a5696bb52742c04d
judigot <judigot@github.com>
2023-08-25 06:11:40 +0800

    Feat: improve UI

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,13 @@ function App() {
 
   const [remainingMoves, setRemainingMoves] = React.useState<number>(9);
 
+  const [message, setMessage] = React.useState<string>("");
+
+  const [isGameOver, setIsGameOver] = React.useState<boolean>(false);
+
   const handleReset = () => {
+    setMessage("");
+    setIsGameOver(false);
     setBoard(CLEAN_BOARD);
     setRemainingMoves(9);
   };
@@ -35,34 +41,45 @@ function App() {
       (board[0][0]=== player && board[1][1]=== player && board[2][2] === player) || // \
       (board[0][2]=== player && board[1][1]=== player && board[2][0] === player) // /
     ) {
+      setIsGameOver(true)
       return true;
     }
   };
 
   const handleMarkGrid = (gridIndex: number) => {
-    const mark = remainingMoves % 2 !== 0 ? PLAYER_ONE : PLAYER_TWO;
-    setRemainingMoves(remainingMoves - 1);
+    if (!isGameOver) {
+      const mark = remainingMoves % 2 !== 0 ? PLAYER_ONE : PLAYER_TWO;
+      setRemainingMoves(remainingMoves - 1);
 
-    // 1st row if index is 0, 1, 2
-    if (gridIndex >= 0 && gridIndex <= 2) {
-      board[0][gridIndex] = mark;
-    }
+      // 1st row if index is 0, 1, 2
+      if (gridIndex >= 0 && gridIndex <= 2) {
+        board[0][gridIndex] = mark;
+      }
 
-    // 2nd row if index is 3, 4, 5
-    if (gridIndex >= 3 && gridIndex <= 5) {
-      board[1][gridIndex - 3] = mark;
-    }
+      // 2nd row if index is 3, 4, 5
+      if (gridIndex >= 3 && gridIndex <= 5) {
+        board[1][gridIndex - 3] = mark;
+      }
 
-    // 3rd row if index is 6, 7, 8
-    if (gridIndex >= 6 && gridIndex <= 8) {
-      board[2][gridIndex - 6] = mark;
-    }
+      // 3rd row if index is 6, 7, 8
+      if (gridIndex >= 6 && gridIndex <= 8) {
+        board[2][gridIndex - 6] = mark;
+      }
 
-    if (checkForWinners("X")) {
-      alert("Player 1 wins");
-    }
-    if (checkForWinners("O")) {
-      alert("Player 2 wins");
+      if (checkForWinners("X")) {
+        setMessage("Player 1 wins");
+      } else {
+        if (remainingMoves === 0) {
+          setMessage("Draw");
+        }
+      }
+      if (checkForWinners("O")) {
+        setMessage("Player 2 wins");
+      } else {
+        if (remainingMoves === 0) {
+          setMessage("Draw");
+        }
+      }
     }
   };
 
@@ -70,18 +87,15 @@ function App() {
     <>
       <h1>Tic-Tac-Toe - judigot</h1>
       <h1>
-        {remainingMoves === 0 &&
-          (() => {
-            return `Draw`;
-          })()}
-        {remainingMoves !== 0 &&
-          (() => {
-            return `${remainingMoves % 2 !== 0 ? "❌" : "⭕"}'s turn`;
-          })()}
+        {message}
+        {!isGameOver && remainingMoves === 0 && "Draw"}
+        {!isGameOver &&
+          remainingMoves !== 0 &&
+          `${remainingMoves % 2 !== 0 ? "❌" : "⭕"}'s turn`}
       </h1>
       <div>Remaining moves: {remainingMoves}</div>
       <div id="gridContainer">
-        <div id="gridBox">
+        <div id="gridBox" className={`${!isGameOver ? "pointer" : ""}`}>
           {[...Array(9)].map((element, i) => {
             let gridState: boolean | string = false;
 
